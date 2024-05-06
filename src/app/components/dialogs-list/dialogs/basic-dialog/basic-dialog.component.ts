abc093ba86f748a40f0a77fec6fe53df0fdd970b
Danil <45513393+Pojerman@users.noreply.github.com>
2024-05-06 17:50:48 +0300

    update basic-dialog

--- src/app/components/dialogs-list/dialogs/basic-dialog/basic-dialog.component.ts
+++ src/app/components/dialogs-list/dialogs/basic-dialog/basic-dialog.component.ts
@@ -7,11 +7,14 @@ import {
   MatDialogTitle,
 } from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-basic-dialog',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './basic-dialog.component.html',
   styleUrl: './basic-dialog.component.scss'
 })
