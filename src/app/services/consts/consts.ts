b87ca77b56a25681b46dfe75fc23882abc3bd8c3
Pojerman <danil110555@mail.ru>
2025-06-25 15:31:28 +0300

    delete services

--- src/app/services/consts/consts.ts
+++
@@ -1,3 +0,0 @@
-export const THEME_KEY = 'theme_key';
-
-export const PREFERS_COLOR_SCHEME = '(prefers-color-scheme)';
--- src/app/services/interfaces/dialogs.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-export interface profileForm {
-  firstName: FormControl<string>;
-  lastName: FormControl<string>;
-  about?: FormControl<string>;
-  phone?: FormControl<string>;
-  description?: FormControl<string>;
-}
