079c881b7723d472a69c19284c0a8bf56ca7afe1
Pojerman <danil110555@mail.ru>
2025-06-25 15:53:29 +0300

    add address-form.component for test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddressFormComponent } from './address-form/address-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddressFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
